6189542fccd146b12cd14fa27e6c2848bdf93786
hikita-toshiki <hikita.toshiki@sunborn.co.jp>
2025-03-16 02:49:46 +0900

    requirements.txtに新しい依存関係を追加し、settings.pyにWhitenoiseミドルウェアを追加

--- project/settings.py
+++ project/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 追加
 ]
 
 ROOT_URLCONF = 'project.urls'
