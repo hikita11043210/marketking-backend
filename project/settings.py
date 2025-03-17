138900d5e9318d51366f681bab131144d26c1363
hikita-toshiki <hikita.toshiki@sunborn.co.jp>
2025-03-17 17:26:57 +0900

    bug

--- project/settings.py
+++ project/settings.py
@@ -140,10 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = []  # 空のリストに変更
 
 # Simplified static file serving
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
